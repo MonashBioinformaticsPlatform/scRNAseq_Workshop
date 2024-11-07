aa42c43208cbc7e4b34ce33af9ee7f716e97746e
lper0012 <lper0012@bio1.novalocal>
2024-11-07 16:46:10 +1100

    build script changed

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ rm -rf docs
 mkdir docs
 
 # Create docs/workshop.R
-/persistent/software/apps/R/4.4.0/bin/Rscript scripts/make_R_script.R
+Rscript scripts/make_R_script.R
 
 
-/persistent/software/apps/R/4.4.0/bin/Rscript -e 'bookdown::render_book("index.Rmd")'
+Rscript -e 'bookdown::render_book("index.Rmd")'
 
